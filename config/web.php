21ee92b0b4a30fa022dbf587a31af900359ae62c
rolion <roleonm@gmail.com>
2015-09-26 22:24:52 -0400

    cambio de pwd

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'roscarlion07',
+            'cookieValidationKey' => '123456789',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
